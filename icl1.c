9f0933c43009d95a20e932d090f1871a79f836d9
Lee <leep9@debian-9.3.0-template>
2018-04-03 11:00:25 -0700

    Initial commit, added icl1.c which implements Hello World.

---
+++ icl1.c
@@ -0,0 +1,10 @@
+/* main returns an integer */
+int main(int argc, char *argv[]) {
+    /* printf is our output function;
+    by default, writes to standard out */
+    /* printf returns an integer, but we ignore that */
+    printf("hello, world\n");
+
+    /* return 0 to indicate all went well */
+    return(0);
+}
