38f9234413995b422b1431af5d1886a7c3ff8021
Lee <leep9@debian-9.3.0-template>
2018-04-03 11:17:48 -0700

    Added code to oepn file leep9.txt which contains a quote.

--- icl1.c
+++ icl1.c
@@ -1,9 +1,21 @@
+#include <stdio.h>
+#include <usistd.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <sys/types.h>
+
 /* main returns an integer */
 int main(int argc, char *argv[]) {
     /* printf is our output function;
     by default, writes to standard out */
     /* printf returns an integer, but we ignore that */
     printf("hello, world\n");
+
+    int fd = open("/tmp/leep9.txt, O_WRONLY | O_CREAT \ O_TRUNC, S_IRWXU);
+    assert(fd > -1);
+    int rc = write(fd, "Operating Systems Programming is amazing\n", 42);
+    assert(rc == 42);
+    close(fd);
 
     /* return 0 to indicate all went well */
     return(0);
